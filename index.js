d2682700591bf1c9cacb459be1a651a03d071e5a
Linda Hrdinová <hrlin@seznam.cz>
2023-09-23 14:21:37 +0200

    zpracovaní samostatného úkolu

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
+/*
+1) Otevřete si naklonovanou složku ve VS Code a prohlédněte si zdrojové HTML. Stránka je nastylována pomocí CSS knihovny Bootstrapu. Co přesně dělají použité CSS třídy není pro toto cvičení podstatné, nemusíte jim věnovat velkou pozornost.
+2) V souboru index.js si na posledním řádku do proměnné uložte kartu s prvním produktem. Pomocí metody classList.add přidejte na tento element třídu border-primary, abychom první produkt na stránce zvýraznili.
+
+3) Do jiné proměnné si uložte tlačítko na druhé kartě. Pomocí metody classList.remove odeberte třídu btn-primary a podívejte se, jak se tlačítko vizuálně změnilo.
+
+4) Do další proměnné si uložte element s třídou card-title posledního produktu. Pomocí voláni metody classList.toggle přidejte na tento element třídu text-center. Text by se měl tímto zarovnat na střed. Vyzkoušejte si, že když tuto metodu zavoláte znova, třída se z prvku odstraní. Takto můžete přepínat mezi přidáním a odebráním třídy pomocí opakovaného volání této metody.
+Na konci by stránka v prohlížeči měla vypadat jako na obrázku níže:
+*/
+
 const product1 = {
   image: 'images/destniklobouk.jpg',
   name: 'Deštníklobouk',
-  description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
+  description:
+    'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
 };
 
 const product2 = {
   image: 'images/slepickabelka.jpg',
   name: 'Slepičkabelka',
-  description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
+  description:
+    'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
 };
 
 const product3 = {
   image: 'images/anatoplavky.jpg',
   name: 'Anatoplavky',
-  description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
+  description:
+    'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+/*
+const produkt1Element = document.querySelector('#product1');
+produkt1Element.classList.add('border-primary');
+
+const produkt2Element = document.querySelector('#product2 button');
+produkt2Element.classList.remove('btn-primary');
+
+const product3Element = document.querySelector('#product3 .card-title');
+product3Element.classList.toggle('text-center');*/
+
+const product1HTML = `
+  <img class="card-img-top" src="${product1.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+const product2HTML = `
+  <img class="card-img-top" src="${product2.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product2.name}</h2>
+    <p class="card-text">${product2.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+const product3HTML = `
+  <img class="card-img-top" src="${product3.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product3.name}</h2>
+    <p class="card-text">${product3.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+const product1Obsah = document.querySelector('#product1');
+const product2Obsah = document.querySelector('#product2');
+const product3Obsah = document.querySelector('#product3');
+product1Obsah.innerHTML = product1HTML;
+product2Obsah.innerHTML = product2HTML;
+product3Obsah.innerHTML = product3HTML;
